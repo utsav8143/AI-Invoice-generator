7eafe3c14acf1b650260717ea08240b48bd2a898
utsav8143 <utsav8143@gmail.com>
2026-06-27 15:15:44 +0530

    change in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const express=require("express");
 const cors=require("cors");
 const path=require("path");
 const connectDB=require("./config/db");
-
-const app=express();
-app.use(express.json());
-
 const authRoutes=require("./routes/authRoute");
 const invoiceRoutes=require("./routes/invoiceRoute");
 const aiRoutes=require("./routes/aiRoute");
+
+const app=express();
+
+
 
 
 
@@ -33,6 +33,13 @@ app.use(express.json());
 app.use("/api/auth",authRoutes);
 app.use("/api/invoices",invoiceRoutes);
 app.use("/api/ai",aiRoutes);
+
+//Serve react frontend static files
+app.use(express.static(path.join(__dirname,"dist")));
+
+app.get("*",(req,res) =>{
+    res.send(path.join(__dirname,"dist","index.html"));
+})
 
 //Start Server
 const PORT=process.env.PORT || 5000;
